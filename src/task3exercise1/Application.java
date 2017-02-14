84e7d146c452e23f4a66504fa09c70e7b804ad92
Muhammad Adli Rachman <adli.rahman23@gmail.com>
2017-02-14 07:05:29 +0700

    Belom Fix

--- src/task3exercise1/Application.java
+++ src/task3exercise1/Application.java
@@ -3,45 +3,61 @@ package task3exercise1;
 public class Application {
 
     // 1. Declare your private attributes here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
-    //
-    //------------------- your code here-----------------------
-    
-    //---------------------------------------------------------
+    // NIM : 1301150040
+    // NAMA : M Adli Rachman  
+        private String appName;
+        private int appSize;
     
     // 2. Declare Constructor here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301150040 
+    // NAMA : .M Adli Rachman  
     //
     // Constructor initialize application name and size (MB)
     //
     //------------------- your code here-----------------------
-    
+        public void Application(String appName, int appSize){
+            this.appName = appName;
+            this.appSize = appSize;
+        }
     //---------------------------------------------------------
     
-    
+
     // 3. Declare your Setter and Getter method here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301150040
+    // NAMA : M Adli Rachman 
     //
     //------------------- your code here-----------------------
-    
+    public String getAppName() {
+        return appName;
+    }    
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public int getAppSize() {
+        return appSize;
+    }
+    public void setAppSize(int appSize) {        
+        this.appSize = appSize;
+    }
+
     //---------------------------------------------------------
-    
-    
     // 4. Create method toString() : String here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301150040
+    // NAMA : M Adli Rachman 
     //
-    // returns String that contains application name and application size 
+    // returns String that contains application name and application size
     // 
     // example : "Application name: App1, with size: 3MB"
     public String toString() {
-    //------------------- your code here-----------------------
+        //------------------- your code here-----------------------
+        System.out.println("Application");
+        System.out.println("Name :"+appName);
+        System.out.println("Size :"+appSize);
         return null;
-    
-    //---------------------------------------------------------
+        
+        //---------------------------------------------------------
     }
 
 }
--- src/task3exercise2/Member.java
+++ src/task3exercise2/Member.java
@@ -3,45 +3,76 @@ package task3exercise2;
 public class Member {
 
     // 1. Declare your private attributes here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301150040 
+    // NAMA : M Adli Rachman 
     //
     //------------------- your code here-----------------------
-    
+        private String name;
+        private String special;
+        private int numOfProject;
     //---------------------------------------------------------
     
     // 2. Declare your 2 Constructor here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301150040 
+    // NAMA : M Adli Rachman 
     //
     //------------------- your code here-----------------------
+    public void Member(String name){
+        this.name = name;
+    }
     
+    public void Member(String name, String special){
+        this.name = name;
+        this.special = special;
+    }
     //---------------------------------------------------------
     
-    
+
     // 3. Declare your Setter and Getter method here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301150040
+    // NAMA : M Adli Rachman 
     //
     //------------------- your code here-----------------------
+    public String getName() {
+        return name;
     
+    }    
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecial() {
+        return special;
+    }
+
+    public void setSpecial(String special) {
+        this.special = special;
+    }
+
+    public int getNumOfProject() {
+        return numOfProject;
+    }
+    public void setNumOfProject(int numOfProject) {        
+        this.numOfProject = numOfProject;
+    }
+
     //---------------------------------------------------------
-    
-    
     // 4. Create method toString() : String here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301150040
+    // NAMA : M Adli Rachman 
     //
-    // returns String that contains member name, specialization, 
-    // and number of project worked  
-    // 
+    // returns String that contains member name, specialization,
+    // and number of project worked
+    //
     // example : "Member Alex specialized in Java Programming, 
     //            worked on 5 project(s)"
     public String toString() {
-    //------------------- your code here-----------------------
+        //------------------- your code here-----------------------
+        System.out.println("Member "+name+" specialized "+special+" Programing and already worked on "+numOfProject+" Project(s)");
         return null;
-    
-    //---------------------------------------------------------
+        
+        //---------------------------------------------------------
     }
 
 }
